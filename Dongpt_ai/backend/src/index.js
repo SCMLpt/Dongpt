fd5ca9c57c3c15341f4465979f2ac60cda588a43
DongPT <69299353+SCMLpt@users.noreply.github.com>
2025-03-01 08:29:04 +0900

    Create index.js

---
+++ Dongpt_ai/backend/src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const DongptCore = require('./core');
+const TextProcessor = require('./plugins/text-processor');
+
+const app = express();
+app.use(express.json());
+
+const core = new DongptCore();
+core.registerPlugin(new TextProcessor());
+
+app.post('/process', async (req, res) => {
+  const context = req.body;
+  await core.run(context);
+  res.json(context);
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
